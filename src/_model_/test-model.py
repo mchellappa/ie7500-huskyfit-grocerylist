f2e9ba7fea3d0c49ce0a776261147b318a132444
MuthuChellappa <mchellappa@jhancock.com>
2025-03-07 19:37:55 -0500

    clean branch

--- src/_model_/test-model.py
+++
@@ -1,6 +0,0 @@
-import spacy
-
-nlp = spacy.load("./output/model-best")
-doc = nlp("½ pound andouille or jalapeno chicken sausages")
-for ent in doc.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
--- src/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from src.api.v1.endpoints.recipe import router as recipe_router
-
-app = FastAPI()
-
-app.include_router(recipe_router, prefix="/api/v1")
